bc0fe06a24f3d3d0b5d88be87dc2862c0c8268d5
Stephen McGranahan <somtwo@gmail.com>
2016-09-04 23:24:39 -0500

    Easy part is mostly done

--- fileProcessor.js
+++ fileProcessor.js
@@ -2,29 +2,60 @@ const assert = require('assert');
 const _ = require('lodash');
 const fs = require('fs');
 
-function _writeIndentation() {
-	for(var i = 0; i < this.indentation; ++i)
-		this.text += "\t";
+function _writeIndentation(output) {
+	for(var i = 0; i < output.indentation; ++i)
+		output.text += "\t";
 }
 
-function _outdent() {
-	assert(this.indentation > 0, "outdent called on a file output that was already at 0 indentation level");
+function _outdent(output) {
+	assert(output.indentation > 0, "outdent called on a file output that was already at 0 indentation level");
 
-	this.indentation--;
-	this.writeIndentation();
-	this.text += "}"
+	output.indentation--;
+	_writeIndentation(output);
+	output.text += "}\n"
+}
+
+function getNamespaceName(moduleName) {
+	var camelCased = moduleName.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); });
+	return camelCased.replace(/^[a-z]/g, function(g) { return g[0].toUpperCase(); });
 }
 
 function generateFileText(output) {
-	return "declare namespace Foo {\n"
-		+ "	export interface IFooProps extends React.Props<Foo> {\n"
-		+ "		foo: boolean;\n"
-		+ "	}\n"
-		+ "	export class Foo extends React.Component<IFooProps, {}> {}\n"
-		+ "}\n"
-		+ 'declare module "foo" {\n'
-		+ "	export = Foo;\n"
-		+ "}\n"
+	var namespace = getNamespaceName(output.moduleName);
+
+	output.indentation = 0;
+	output.text = ``;
+	output.text += `declare namespace ${namespace} {\n`;
+	output.indentation++;
+
+	_.each(output.interfaces, function(element, key) {
+		_writeIndentation(output);
+		output.text += `export interface ${element.name} extends React.Props<${element.componentClassName}> {\n`;
+		output.indentation ++;
+
+		_.each(element.members, function(member, key) {
+			_writeIndentation(output);
+			output.text += `${member.name}: ${member.type};\n`;
+		});
+
+		_outdent(output);
+	});
+
+	_.each(output.classes, function(element, key) {
+		_writeIndentation(output);
+		output.text += `export class ${element.name} extends React.Component<${element.propsInterface}, {}> {\n`;
+		output.indentation++;
+
+		// TODO: Do we ever need to put anything in this?
+
+		_outdent(output);
+	});
+
+	_outdent(output);
+
+	output.text += `declare module "${output.moduleName}" {\n`
+	output.text += `	export = ${namespace};\n`;
+	output.text += `}\n`;
 }
 
 // TODO: roll output file name if output file already exists
@@ -34,29 +65,43 @@ function writeTypeFile(fileName, outputText) {
 
 // TODO: Reference the base react.d.ts file
 function processFile(fileName) {
-	var output = [
-		{
-			'name': 'IFooProps',
-			'members': [{'name': 'foo', 'type': 'string'}],
-		}
-	];
+	// TODO: Somethign valid
+	var output = {
+		'interfaces': [
+			{
+				'name': 'IFooProps',
+				'componentClassName': 'Foo',
+				'members': [{'name': 'foo', 'type': 'string'}],
+			}
+		],
+		'classes': [
+			{
+				'name': 'Foo',
+				'propsInterface': 'IFooProps'
+			}
+		]
+	};
 
 	return output;
 }
 
 function processFiles(fileNames) {
-	var interfaces = _.map(fileNames, function(key, value) {
+	var processedFiles = _.map(fileNames, function(value) {
 		return processFile(value);
 	});
+
+	var interfaces = _.map(processedFiles, function(value) { return value.interfaces; });
+	var classes = _.map(processedFiles, function(value) { return value.classes; });
 
 	// TODO: Determine what the namespace should be
 	var output = {
 		'moduleName': 'foo-module',
-		'interfaces': _.flatten(interfaces)
+		'interfaces': _.flatten(interfaces),
+		'classes': _.flatten(classes)
 	};
 
-	var outputText = generateFileText(output);
-	writeTypeFile("output.d.ts", outputText);
+	generateFileText(output);
+	writeTypeFile("output.d.ts", output.text);
 }
 
 module.exports = {
